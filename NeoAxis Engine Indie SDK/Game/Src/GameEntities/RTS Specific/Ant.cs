d6b0c09b76230a31ca9c426258eae127121693d4
Darren <darren.m.hassan@googlemail.com>
2010-08-26 20:53:48 +0800

    Added an animated ant to the huge map

---
+++ NeoAxis Engine Indie SDK/Game/Src/GameEntities/RTS Specific/Ant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace GameEntities.RTS_Specific
+{
+    public class AntType : RTSCharacterType
+    {
+        /*[FieldSerialize]
+        [DefaultValue("Walk")]
+        string walkAnimationName = "Walk";
+        
+        [DefaultValue("Walk")]
+        public new string WalkAnimationName
+        {
+            get { return walkAnimationName; }
+            set { walkAnimationName = value; }
+        }*/
+    }
+    public class Ant : RTSCharacter
+    {
+        AntType _type = null; public new AntType Type { get { return _type; } }
+
+        protected override void OnPostCreate(bool loaded)
+        {
+            base.OnPostCreate(loaded);
+        }
+    }
+}
