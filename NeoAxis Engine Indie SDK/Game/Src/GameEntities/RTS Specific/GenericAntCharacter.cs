c211b10eb6770347b9324c9443bcaf694cbb5cb4
Darren <darren.m.hassan@googlemail.com>
2010-09-02 08:38:55 +0800

    Created a generic ant character from which specific ants (such as foragers) can inherit and modified the map to contain generic ant types. BTW the generic ant already has something resembling an Attack mode

---
+++ NeoAxis Engine Indie SDK/Game/Src/GameEntities/RTS Specific/GenericAntCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace GameEntities.RTS_Specific
+{
+    public class GenericAntCharacterType : RTSCharacterType
+    {
+    }
+    public class GenericAntCharacter : RTSCharacter
+    {
+        GenericAntCharacterType _type = null; public new GenericAntCharacterType Type { get { return _type; } }
+
+        protected override void OnPostCreate(bool loaded)
+        {
+            base.OnPostCreate(loaded);
+        }
+    }
+}
